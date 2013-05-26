0dfb740aa8776209fe997fe1794dfe29ce2869fe
Niels Abildgaard <niels.abildgaard@gmail.com>
2013-05-26 19:08:32 +0200

    Oops: Another broken ref fixed.

--- public_html/manager.php
+++ public_html/manager.php
@@ -6,7 +6,7 @@ require_once "../backend/models/Tweet.php";
     <head>
         <meta charset="utf-8">
         <title>Manager | Heard at ITU</title>
-        <link href="bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="/vendor/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,400italic,700,700italic' rel='stylesheet' type='text/css'>
         <link href="/res/style.css" rel="stylesheet" type="text/css">
     </head>
