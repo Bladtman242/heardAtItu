04b9b775b385dcd3a4c340e222da0afd21b93d14
Niels Abildgaard <niels.abildgaard@gmail.com>
2013-05-27 22:23:26 +0200

    Bit more work lulz

--- backend/delegator.php
+++ backend/delegator.php
@@ -4,7 +4,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 //Setup Path namespace
 class Path {
     public static $BACKEND = "../backend";
@@ -12,6 +11,18 @@ class Path {
     public static $VIEWS = "../backend/views";
 }
 
+//Autoload
+function __autoload($class) {
+    echo "Autoloading ".$class;
+    include "/controllers/".$class.".php";
+}
+
 $config = json_decode(file_get_contents(Path::$BACKEND."/config.json"));
+
+$controller_name = $config->defaultController."controller";
+
+$controller = new $controller_name;
+$controller->setUp();
+$controller->render();
 
 ?>
