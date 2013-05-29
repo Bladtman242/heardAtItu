a7a1731e97a238a90422c217a9d982d9efdb10e4
Sigurt "Akavet" Bladt Dinesen <bladt@deranged.(none)>
2013-05-29 23:56:37 +0000

    deleted:    index2.php

--- public_html/index2.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once "../backend/framework/GeneralModel.php";
-require_once "../backend/models/ManagerModel.php";
-
-echo $_GET['u']." ".$_GET['p'];
-ManagerModel->authenticate($_GET['u'],$_GET['p']);
-?>
