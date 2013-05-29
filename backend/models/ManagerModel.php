b3868c80b63cbaea277e7ade83272cd47514cf5d
Sigurt "Akavet" Bladt Dinesen <bladt@deranged.(none)>
2013-05-29 23:29:28 +0000

    modified:   backend/models/ManagerModel.php 	modified:   public_html/index2.php

--- backend/models/ManagerModel.php
+++ backend/models/ManagerModel.php
@@ -1,16 +1,15 @@
 <?php
-
 class ManagerModel extends GeneralModel {
 
     public function setUp() {
         parent::setUp();
     }
     
-    private function authenticate($username, $passwd) {
+    public static function authenticate($username, $passwd) {
         $mysqli = $this->getMysqli();
 	$res = $mysqli->query("SELECT password as p FROM managers WHERE username = $username");
 	if ($res){
-	    if($res['p'] == sha256(sha256($username).passwd){
+	    if($res['p'] == sha256(sha256($username).passwd)){
 		return true; 
 	    }
 	}
