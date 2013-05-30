28aa80ac36f670134f4c94d05177783e61c1601a
Sigurt <sigurt91@gmail.com>
2013-05-31 00:56:28 +0200

    pathetic attempt at styling

--- backend/views/login.php
+++ backend/views/login.php
@@ -1,5 +1,19 @@
+<style type='text/css'>
+    input {
+	float: right;
+    }
+
+    button {
+	float: right;
+    }
+
+    form {
+        line-height: 2em;
+    }
+
+</style>
 <form method="post">
-    Username: <input type="text" name="user" required><br>
-    Password: <input type="password" name="pwd" required>
+    <label>Username: </label><input type="text" name="user" required><br>
+    <label>Password: </label> <input type="password" name="pwd" required><br>
     <button class="btn btn-primary" id="authorize-button">Authorize</button>
 </form>
