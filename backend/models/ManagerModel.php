7adedeb573f2e849f7109cb5fa78deeadd9b4355
Sigurt <sigurt91@gmail.com>
2013-05-30 19:42:59 +0200

    Prettied the if($res).. up - subjectively. We might explicit error messages though. (Why could we not authenticate, db-error or just bad password? bad user?) modified:   backend/models/ManagerModel.php

--- backend/models/ManagerModel.php
+++ backend/models/ManagerModel.php
@@ -9,16 +9,10 @@ class ManagerModel extends GeneralModel {
         $mysqli = $this->getMysqli();
 	$res = $mysqli->query("SELECT password AS p FROM managers WHERE username = '$username'");
 	$res = $res->fetch_assoc();
-	if ($res){
-	    echo "lars";
-	    if($res['p'] == hash('sha256',hash('sha256',($username)).$passwd)){
-	        echo "lars2";
-		return true; 
-	    }
+	if($res && $res['p'] == hash('sha256',hash('sha256',($username)).$passwd)){
+	    return true; 
 	}
-	echo "larsf";
         return false;
     }
 }
-
 ?>
