df48c77b1b61c5b43d60be363553a32e70b2f195
Sigurt <sigurt91@gmail.com>
2013-05-31 19:51:59 +0200

    Manager-mail implemented, though the getopts could still use polishing

--- serviceUtils.php
+++ serviceUtils.php
@@ -10,6 +10,10 @@ require_once "backend/models/TwitterModel.php";
 require_once "backend/components/Tweet.php";
 require_once "vendor/twitteroauth/twitteroauth.php";
 require_once "vendor/twitteroauth/OAuth.php";
+ 
+$managers = "Sigurt <sigurt91@gmail.com>, Niels <niels.abildgaard@gmail.com>";
+$mailSubject = "Pending tweets on heard.atItu.dk.";
+$mailBody = "There are pending tweets on heard.atItu.dk";
 
 class ServiceUtils{
     private $model;
@@ -29,13 +33,28 @@ class ServiceUtils{
         }
         else echo "No availible tweets\n";
     }
+
+    public function mailManagers() {
+        global $managers, $mailSubject, $mailBody;
+
+        $tw = $this->model->GetAll(TwitterModel::$STATE_PENDING);
+        if(is_array($tw) && count($tw)>0){
+            echo mail($managers, $mailSubject, $mailBody) ? "Mail sent to managers\n" : "Mailing error\n";
+        } else {
+            echo "no pending tweets, no mails sent";
+        }
+    }
 }
 
-$opt = getopt("p");
+$opt = getopt("pm");
 
 $su = new ServiceUtils();
 
 if (isset($opt['p'])) {
     $su->postTweet();
+}
+
+if (isset($opt['m'])) {
+    $su->mailManagers();
 }
 ?>
