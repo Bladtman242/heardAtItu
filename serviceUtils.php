b04745a7cd0abef4e417596e019d219e8287f262
Sigurt <sigurt91@gmail.com>
2013-05-31 19:03:31 +0200

    renamed postOne to serviceUtils. serviceUtils is a class, and has wrapping so that it can be called properly from the commandline i.e. ./serviceUtils.php instead of php servi....

--- serviceUtils.php
+++ serviceUtils.php
@@ -5,12 +5,19 @@ require_once "backend/Tweet.php";
 require_once "vendor/twitteroauth/twitteroauth.php";
 require_once "vendor/twitteroauth/OAuth.php";
 
-$tw = Tweet::LoadMostRecent(Tweet::$STATE_APPROVED);
-if($tw != null) {
-    if($tw->send()) {
-        echo "Sent tweet #{$tw->getId()}:\n{$tw->content}\n";
+class ServiceUtils{
+    public function postTweet() {
+        $tw = Tweet::LoadMostRecent(Tweet::$STATE_APPROVED);
+        if($tw != null) {
+            if($tw->send()) {
+                echo "Sent tweet #{$tw->getId()}:\n{$tw->content}\n";
+            }
+            else echo "Failed to send tweet #{$tw->getId()}:\n{$tw->content}\n";
+        }
+        else echo "No availible tweets\n";
     }
-    else echo "Failed to send tweet #{$tw->getId()}:\n{$tw->content}\n";
 }
-else echo "No availible tweets\n";
+
+$su = new ServiceUtils();
+$su->postTweet();
 ?>
