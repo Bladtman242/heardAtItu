306bbc91f17c29b2291def2dbaa48e1a198fb048
Niels Abildgaard <niels.abildgaard@gmail.com>
2013-05-31 16:16:41 +0200

    Styled login view

--- backend/views/login.php
+++ backend/views/login.php
@@ -1,19 +1,5 @@
-<style type='text/css'>
-    input {
-	float: right;
-    }
-
-    button {
-	float: right;
-    }
-
-    form {
-        line-height: 2em;
-    }
-
-</style>
-<form method="post">
-    <label>Username: </label><input type="text" name="user" required><br>
-    <label>Password: </label> <input type="password" name="pwd" required><br>
-    <button class="btn btn-primary" id="authorize-button">Authorize</button>
+<form method="post" id="login-form">
+    <input type="text" name="user" required placeholder="Username"><br>
+    <input type="password" name="pwd" required placeholder="Password"><br>
+    <button class="btn btn-primary">Authorize</button>
 </form>
