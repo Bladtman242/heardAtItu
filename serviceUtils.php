d525fa7eb7e4b907747b0d274b162d54adbf9a7d
Sigurt <sigurt91@gmail.com>
2013-05-31 18:48:09 +0200

    new file:   serviceUtils.php

---
+++ serviceUtils.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+error_reporting(E_ALL);
+require_once "backend/Tweet.php";
+require_once "vendor/twitteroauth/twitteroauth.php";
+require_once "vendor/twitteroauth/OAuth.php";
+
+$tw = Tweet::LoadMostRecent(Tweet::$STATE_APPROVED);
+if($tw != null) {
+    if($tw->send()) {
+        echo "Sent tweet #{$tw->getId()}:\n{$tw->content}\n";
+    }
+    else echo "Failed to send tweet #{$tw->getId()}:\n{$tw->content}\n";
+}
+else echo "No availible tweets\n";
+?>
